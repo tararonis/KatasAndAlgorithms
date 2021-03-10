550e8004cd3076fc5abed7d37d437fb2e6c8705e
tararonis <tararonis@gmail.com>
2021-03-10 08:09:22 +0200

    Added new Kata-hIndex

--- Python/CodeWars/?5. Regex.py
+++
@@ -1,3 +0,0 @@
-#https://www.codewars.com/kata/52e1476c8147a7547a000811/train/python
-
-regex=""
---
+++ Python/LetsCode/274. h-index.py
@@ -0,0 +1,46 @@
+#https://leetcode.com/problems/h-index/
+
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        """
+        According to the definition of h-index on Wikipedia:
+         "A scientist has index h if h of his/her N papers have
+          at least h citations each, and the other N − h papers 
+          have no more than h citations each."
+
+        Args:
+            citations (List[int]): input
+
+        Returns:
+            int: [description]
+        """
+        n = len(citations)
+        cit_list = [0] * (n + 1)
+
+        for cit in citations:
+            if cit < n:
+                cit_list[cit] +=1
+            else:
+                cit_list[n] +=1
+
+        total = 0
+        i = n
+
+        while i <= 0:
+            total += cit_list[n]
+            if total >= i:
+                return i
+            i -= 1
+        return i
+
+
+def main():
+    citations = [3,0,6,1,5]
+    s = Solution
+    s.hIndex(citations)
+
+
+if __name__ == "__main__":
+    main()
